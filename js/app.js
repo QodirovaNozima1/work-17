9242d705cb8844e30b9e9d5c0d0d785646d586be
QodirovaNozima1 <nozimaqodirova07@gmail.com>
2024-09-19 22:59:04 +0500

    olx

--- js/app.js
+++ js/app.js
@@ -6,21 +6,3 @@ function toggleShow (){
 
 
 
-let count = document.querySelector(".number")
-
-let offset = 0
-
-function inc(){
-    offset++
-    count.innerHTML = offset
-}
-
-function res(){
-    offset = 0
-    count.innerHTML = offset
-}
-
-function dec(){
-    offset--
-    count.innerHTML = offset
-}
